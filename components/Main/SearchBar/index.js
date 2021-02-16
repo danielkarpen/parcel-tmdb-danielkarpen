7e3173a0c0a420e6578d451e082f64cd6a2e0838
Daniel <danielkarpen97@gmail.com>
2021-02-16 14:25:59 -0600

    add searchbar component

---
+++ components/Main/SearchBar/index.js
@@ -0,0 +1,5 @@
+export default () => `
+<div>
+  <label for="search">Search</label>
+  <input type="search" placeholder="Search for a movie!" id="search" />
+</div>`;
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import api from "./api";
+import { Search } from "./components";
 
 api.index();
+
+const root = document.querySelector("#root");
+
+function render() {
+  root.innerHTML = Search();
+}
+
+render();
